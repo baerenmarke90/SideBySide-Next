cf72246e02699d36180bb5b7da029104d940ff5e
baerenmarke90 <49028921+baerenmarke90@users.noreply.github.com>
2026-08-30 21:22:07 +0200

    style(web): apply Biome formatting (#298)

--- web/src/client/peopleApi.ts
+++ web/src/client/peopleApi.ts
@@ -1,7 +1,10 @@
 import { PeopleApi } from '../api/generated/apis/PeopleApi';
 import { Configuration } from '../api/generated/runtime';
 
-export function createPeopleApi(apiBaseUrl: string, accessToken: string): PeopleApi {
+export function createPeopleApi(
+  apiBaseUrl: string,
+  accessToken: string,
+): PeopleApi {
   return new PeopleApi(
     new Configuration({
       basePath: apiBaseUrl,
