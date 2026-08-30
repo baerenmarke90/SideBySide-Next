fb29bea3eb3613af2025189ee64e73ac64f0a52b
baerenmarke90 <49028921+baerenmarke90@users.noreply.github.com>
2026-08-30 21:41:06 +0200

    style(web): load profile page styles (#298)

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -11,6 +11,7 @@ import './theme.css';
 import './shell.css';
 import './attachment-drafts.css';
 import './components/RelatedPeoplePage.css';
+import './components/ProfilePage.css';
 
 initializeTheme();
 
